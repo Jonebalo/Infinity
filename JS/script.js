66407d02b1d49cbd51d8347eb48601048c9ff995
@jonebalo <jonebalo@hotmail.com>
2023-09-25 16:11:20 +0200

    Algunos cambios

---
+++ JS/script.js
@@ -0,0 +1,30 @@
+// document.getElementById('search-form').addEventListener('submit', function (e) {
+//     e.preventDefault();
+    
+//     const searchQuery = document.getElementById('search-input').value.toLowerCase();
+//     const products = [
+//         { name: 'Producto 1', price: 19.99 },
+//         { name: 'Producto 2', price: 29.99 },
+//         { name: 'Producto 3', price: 39.99 },
+//         // Agrega más productos a tu base de datos
+//     ];
+
+//     const results = products.filter(product => product.name.toLowerCase().includes(searchQuery));
+
+//     displayResults(results);
+// });
+
+// function displayResults(results) {
+//     const resultsContainer = document.getElementById('search-results');
+//     resultsContainer.innerHTML = '';
+
+//     if (results.length === 0) {
+//         resultsContainer.innerHTML = 'No se encontraron resultados.';
+//     } else {
+//         results.forEach(product => {
+//             const productElement = document.createElement('div');
+//             productElement.innerHTML = `<p>${product.name} - $${product.price}</p>`;
+//             resultsContainer.appendChild(productElement);
+//         });
+//     }
+// }
